934acc2f8543689ecf3cfbc6e62492c09213ed5a
ljt528 <ljt528@naver.com>
2023-11-02 01:54:01 +0900

    Build : Properties 설정 수정

--- src/test/java/com/shop/repository/ItemRepositoryTest.java
+++ src/test/java/com/shop/repository/ItemRepositoryTest.java
@@ -8,12 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
-//import static org.junit.jupiter.api.Assertions.*;
-
 import java.time.LocalDateTime;
 
 @SpringBootTest
-@TestPropertySource(locations = "classpath:application-test.properties")
+@TestPropertySource(locations="classpath:application-test.properties")
 class ItemRepositoryTest {
 
     @Autowired
@@ -21,7 +19,7 @@ class ItemRepositoryTest {
 
     @Test
     @DisplayName("상품 저장 테스트")
-    public void createItemTest() {
+    public void createItemTest(){
         Item item = new Item();
         item.setItemNm("테스트 상품");
         item.setPrice(10000);
