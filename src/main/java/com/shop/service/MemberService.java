3862a0a731eff95491b044df75bfe773ba575857
ljt528 <ljt528@naver.com>
2023-11-19 02:52:20 +0900

    📝Comment: "추가 코드 설명 주석"

--- src/main/java/com/shop/service/MemberService.java
+++ src/main/java/com/shop/service/MemberService.java
@@ -15,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 // final이나 @NonNull이 붙은 필드에 생성자 생성
 @RequiredArgsConstructor
+// MemberService가 UserDetailsService를 구현
 public class MemberService implements UserDetailsService {
     // 빈에 생성자가 1개이고, 파라미터 타입이 빈으로 등록 가능 시 @Autowired 없이 의존성 주입 가능
     private final MemberRepository memberRepository;
@@ -33,13 +34,14 @@ public class MemberService implements UserDetailsService {
     }
 
     @Override
+    // 로그인할 유저의 email을 파라미터로 전달받는다.
     public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
         Member member = memberRepository.findByEmail(email);
 
         if(member == null) {
             throw new UsernameNotFoundException(email);
         }
-
+        // User 객체를 반환
         return User.builder()
                 .username(member.getEmail())
                 .password(member.getPassword())
