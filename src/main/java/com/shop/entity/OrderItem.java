b79a57d50ff6de9217ae9096e387c2c0c07e2fed
nohheeyeon <heeyeon1213@gmail.com>
2023-11-19 20:35:27 +0900

    🐎Refactor : "BaseEntity 상속받기"

--- src/main/java/com/shop/entity/OrderItem.java
+++ src/main/java/com/shop/entity/OrderItem.java
@@ -9,18 +9,17 @@ import java.time.LocalDateTime;
 @Entity
 @Getter @Setter
 
-public class OrderItem {
+public class OrderItem extends BaseEntity {
 
-    @Id
-    @GeneratedValue
-    @Column(name = "item_id")
+    @Id @GeneratedValue
+    @Column(name = "order_item_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "item_id")
     private Item item;
 
-    @ManyToOne
-    @JoinColumn(name = "order_id")
-    private Item item;
-
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "order_id")
     private Order order;
 
