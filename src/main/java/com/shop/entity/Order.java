c872000d0a08a04f4e13f5692efc82b03a15e441
nohheeyeon <heeyeon1213@gmail.com>
2023-11-19 14:03:39 +0900

    🐎Feat : "@OneToMany 어노테이션에 cascade 옵션 설정"

--- src/main/java/com/shop/entity/Order.java
+++ src/main/java/com/shop/entity/Order.java
@@ -26,4 +26,7 @@ public class Order {
     private LocalDateTime regTime;
 
     private LocalDateTime updateTime;
+
+    @OneToMany(mappedBy = "order", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<OrderItem> orderItems = new ArrayList<>();
 }
