7eeb30a1329e939ccb6364f5b677fa9834d7f6b7
xkimido <kimido987@gmail.com>
2023-11-20 11:02:31 +0900

    🐎Feat: "추가 주문 취소 기능"

---
+++ src/main/java/com/shop/controller/OrderController.java
@@ -0,0 +1,27 @@
+package com.shop.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.security.Principal;
+
+@Controller
+@RequiredArgsConstructor
+public class OrderController {
+
+    @PostMapping("/order/{orderId}/cancel")
+    public @ResponseBody ResponseEntity cancelOrder(@PathVariable("orderId") Long orderId, Principal principal) {
+
+        if (!orderService.validateOrder(orderId, principal.getName())) {
+            return new ResponseEntity<String>("주문 취소 권한이 없습니다.", HttpStatus.FORBIDDEN);
+        }
+
+        orderService.cancelOrder(orderId);
+        return new ResponseEntity<Long>(orderId, HttpStatus.OK);
+    }
+}
