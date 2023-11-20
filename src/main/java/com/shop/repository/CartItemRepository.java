75d1851940fa05588f6b026070cf5b8742ed78f5
xkimido <kimido987@gmail.com>
2023-11-20 11:23:49 +0900

    🐎Feat: "추가 상품 조회 메소드"
    카트 아이디와 상품 아이디를 이용해서 상품이 장바구니에 들어있는지 조회하는 메소드 작성

--- src/main/java/com/shop/repository/CartItemRepository.java
+++ src/main/java/com/shop/repository/CartItemRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CartItemRepository extends JpaRepository<CartItem, Long> {
 
+    CartItem findByCartIdAndItemId(Long cartId, Long itemId);
 }
