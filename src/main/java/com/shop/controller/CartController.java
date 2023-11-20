2848bf8ffc0bedcd2357aafc8624c5b20f63233b
ljt528 <ljt528@naver.com>
2023-11-20 12:50:25 +0900

    🐎Feat: "구현 장바구니 상품의 수량을 업데이트하는 요청 처리 로직"

--- src/main/java/com/shop/controller/CartController.java
+++ src/main/java/com/shop/controller/CartController.java
@@ -2,6 +2,7 @@ package com.shop.controller;
 
 
 import com.shop.dto.CartDetailDto;
+import com.shop.dto.CartOrderDto;
 import com.shop.service.CartService;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
@@ -54,5 +55,28 @@ public class CartController {
 
         cartService.deleteCartItem(cartItemId);
         return new ResponseEntity<Long>(cartItemId, HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/cart/orders")
+    public @ResponseBody ResponseEntity orderCartItem(@RequestBody CartOrderDto cartOrderDto, Principal principal){
+
+        List<CartOrderDto> cartOrderDtoList = cartOrderDto.getCartOrderDtoList();
+
+        // 주문할 상품을 선택하지 않았는지 체크
+        if(cartOrderDtoList == null || cartOrderDtoList.size() == 0){
+            return new ResponseEntity<String>("주문할 상품을 선택해주세요", HttpStatus.FORBIDDEN);
+        }
+
+        // 주문 권한을 체크
+        for (CartOrderDto cartOrder : cartOrderDtoList) {
+            if(!cartService.validateCartItem(cartOrder.getCartItemId(), principal.getName())){
+                return new ResponseEntity<String>("주문 권한이 없습니다.", HttpStatus.FORBIDDEN);
+            }
+        }
+
+        // 주문 로직 호출 결과 생성된 주문 번호를 반환
+        Long orderId = cartService.orderCartItem(cartOrderDtoList, principal.getName());
+        // 생성된 주문 번호와 요청이 성공했다는 HTTP 응답 상태 코드 반환
+        return new ResponseEntity<Long>(orderId, HttpStatus.OK);
     }
 }
