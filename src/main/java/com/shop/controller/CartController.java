f8570e0de0de8a10c39e785c76a54878bf4d6edf
ljt528 <ljt528@naver.com>
2023-11-20 12:08:22 +0900

    🐎Feat: "추가 장바구니 상품의 수량을 업데이트하는 기능"

--- src/main/java/com/shop/controller/CartController.java
+++ src/main/java/com/shop/controller/CartController.java
@@ -2,9 +2,16 @@ package com.shop.controller;
 
 
 import com.shop.dto.CartDetailDto;
+import com.shop.service.CartService;
 import lombok.RequiredArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.security.Principal;
 import java.util.List;
@@ -20,6 +27,23 @@ public class CartController {
         // 조회한 장바구니 상품 정보를 뷰로 전달
         model.addAttribute("cartItems", cartDetailDtoList);
         return "cart/cartList";
+    }
+
+    // 장바구니 상품의 수량만 업데이트
+    @PatchMapping(value = "/cartItem/{cartItemId}")
+    public @ResponseBody ResponseEntity updateCartItem (@PathVarible("cartItemId") Long cartItemId, int count, Principal principal) {
+
+        // 장바구니에 담겨있는 상품의 개수가 0이하일 경우 에러 메시지 반환
+        if (count <= 0) {
+            return new ResponseEntity<String> ("최소 1개 이상 담아주세요", HttpStatus.BAD_REQUEST);
+        // 수정 권한 체크
+        } else if (!CartService.validateCartItem (cartItemId, principal.getName())) {
+            return new ResponseEntity<String> ("수정 권한이 없습니다.", HttpStatus.FORBIDDEN);
+        }
+
+        // 장바구니 상품의 개수 업데이트
+        cartService.updateCartItemCount(cartItemId, count);
+        return new ResponseEntity<Long> (cartItemId, HttpStatus.OK);
     }
 
 }
