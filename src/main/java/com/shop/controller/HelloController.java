be8370bacd08a9120dfc78137bd66bec148fc826
nohheeyeon <heeyeon1213@gmail.com>
2023-12-17 18:31:37 +0900

    🐎Feat : "CORS를 위한 @CrossOrigin 어노테이션 추가"

--- src/main/java/com/shop/controller/HelloController.java
+++ src/main/java/com/shop/controller/HelloController.java
@@ -1,10 +1,12 @@
 package com.shop.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Date;
 
+@CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class HelloController {
 
